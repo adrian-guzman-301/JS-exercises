a362e752b7a4ad00b4efedfc30898b11c96098bb
Adrian Guzman <adrian1217guzman@gmail.com>
2025-10-29 17:56:17 -0700

    write pseudo-code solution for creating a function that takes two numbers as arguments and returns their sum

---
+++ daily-coding-challenges/oct-2025/oct-29/pseudo-code-challenge.js
@@ -0,0 +1,18 @@
+/*
+Create a function that takes two numbers as arguments and returns their sum.
+
+Examples: 	
+addition(3, 2) ➞ 5		addition(-3, -6) ➞ -9	addition(7, 3) ➞ 10
+
+Notes: Don't forget to return the result and check for edge cases! 
+
+
+*/
+
+//parameters: 2 values of type number
+//returns: the sum of the 2 values
+//examples: addition(3, 2) ➞ 5		addition(-3, -6) ➞ -9	addition(7, 3) ➞ 10
+//pseudo-code
+    //step 1: declare function named addition with two parameters, x and y
+    //step 2: declare sum variable, assigning 'x + y' to the sum variable
+    //step 3: return sum variable
