b686f8b6205ccb23cec8dfe97cd6997dc7de061d
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-03 16:44:05 -0800

    learned splice mutates an array while toSpliced does not

---
+++ daily-coding-challenges/nov-2025/nov-03/script.js
@@ -0,0 +1,5 @@
+let pizzaArr = ['cheese', 'pepperoni', 'the works', 'hawaiian']
+
+console.log(pizzaArr.slice(2, 3))
+console.log(pizzaArr.slice(1,3))
+console.log(pizzaArr.toSpliced(1, 0, 'salami'))//spliced changes an array while toSpliced does not
