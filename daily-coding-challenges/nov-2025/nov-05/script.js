21ac3f123fa102b61f35098eb6584d38a5725122
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-05 23:19:59 -0800

    write feedback from today's mock interview

---
+++ daily-coding-challenges/nov-2025/nov-05/script.js
@@ -0,0 +1,8 @@
+//secret pair programming completed
+/*
+feedback to incorporate: 
+  1. be more mindful of my partner
+  2. be aware of edge cases
+  3. keep asking my partner if they're okay
+  4. communicate that i am a silent processor when reading instructions or thinking about a solution
+*/
