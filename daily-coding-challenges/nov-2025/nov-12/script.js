feac444804e0645939aecd3c73560f6e4d8e190f
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-12 18:34:03 -0800

    complete code-along with naomi

---
+++ daily-coding-challenges/nov-2025/nov-12/script.js
@@ -0,0 +1,49 @@
+/*
+Perform a search and replace on the sentence using the arguments provided and return the new sentence. First argument is the sentence to perform the search and replace on. Second argument is the word that you will be replacing (before). Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+
+
+Examples
+myReplace("Let us go to the store", "store", "mall") should return the string ”Let us go to the mall.”
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return the string “He is Sitting on the couch.”
+myReplace("I think we should look up there", "up", "Down") should return the string ”I think we should look down there.”
+myReplace("This has a spellngi error", "spellngi", "spelling") should return the string ”This has a spelling error.”
+myReplace("His name is Tom", "Tom", "john") should return the string His name is John.
+myReplace("Let us get back to more Coding", "Coding", "algorithms") should return the string Let us get back to more Algorithms.
+
+*/
+
+// console.log(myReplace("Let us go to the store", "store", "mall")); // "Let us go to the mall."
+// console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); // "He is Sitting on the couch."
+// console.log(myReplace("I think we should look up there", "up", "Down")); // "I think we should look down there."
+// console.log(myReplace("This has a spellngi error", "spellngi", "spelling")); // "This has a spelling error."
+// console.log(myReplace("His name is Tom", "Tom", "john")); // "His name is John."
+// console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms")
+// ); "Let us get back to more Algorithms."
+
+let original = 'Let us go to the store.';
+let newSentence = original.replace('store', 'mall');
+console.log(newSentence)
+
+//function takes 3 parameters: original sentence, word to remove, word to add
+
+String.prototype.replace()//the replace() method of string values returns a new string with one, some, or all matches of a pattern replaced by a replacement
+
+// function myReplace(sentence, removedWord, insertedWord) {
+//   return sentence.replace(removedWord, insertedWord)
+// }
+//check if word is even there 
+//check for edge cases
+const myReplace = (sentence, original, replacement) => {
+  if(!sentence.includes(original)) {
+    return 'the word does not exist bruv'
+  } else {
+    let firstChar = original[0]//original.charAt(0)
+    let isUpperCased = firstChar === firstChar.toUpperCase()
+    let capitalizedReplacement = replacement.charAt(0).toUpperCase() + replacement.slice(1)
+    let lowerCasedReplacement = replacement.charAt(0).toLowerCase() + replacement.slice(1) 
+    let forReplacing = isUpperCased ? capitalizedReplacement : lowerCasedReplacement
+    return sentence.replace(original, forReplacing)
+  }
+}
