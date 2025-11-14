62721cd0a3183ad3ab060be9434a994674fb9f59
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-13 22:35:36 -0800

    complete naomi code-along

--- daily-coding-challenges/nov-2025/nov-13-oop/script.js
+++ daily-coding-challenges/nov-2025/nov-13-oop/script.js
@@ -14,13 +14,27 @@ class Event {
 
   addAvailableTickets(name, price) {
     // Add your code here
+    const existingTicket = this.availableTickets.find(ticket => ticket.name === name)
+    if(existingTicket) {
+      console.warn(`Ticket type ${name} already exists.`)
+    }
+    this.availableTickets.push({name, price});
   }
 
   allTickets() {
     // Add your code here
+    if(this.availableTickets.length === 0) {
+      return `${this.name} - has no tickets available.`;
+    }
+    const tickets = this.availableTickets.map((ticket, index) => `${ticket.name}: ${ticket.price}`).join(' ')
+    return `${this.name} tickets: ${tickets}`
   }
 
   searchTickets(lower, upper) {
     // Add your code here
   }
 }
+
+//event
+const TechtonicaGradParty = new Event('Techtonica Graduation Party', 'Graduation for the Part 1 grads, December 2025')
+const ThanksgivingDinnerParty = new Event('TDay Bash', 'Party for all folks not going home')
